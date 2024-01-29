9c6cee50c0a6fd93db57e59b76f81874765fe180
Klimova Natalia <skorpnat@gmail.com>
2024-01-29 17:00:49 +0600

    init project

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,37 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import drivers.BrowserstackDriver;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestBase {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.browser = BrowserstackDriver.class.getName();
+        Configuration.browserSize = null;
+        Configuration.timeout = 30000;
+    }
+
+    @AfterEach
+    void addAttachments() {
+        String sessionId = sessionId().toString();
+        Attach.pageSource();
+        closeWebDriver();
+        Attach.addVideo(sessionId);
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        open();
+    }
+
+}
