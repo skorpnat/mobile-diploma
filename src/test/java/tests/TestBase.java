ba22f5d787421c406b7a8ed73180754faaeaf445
Klimova Natalia <skorpnat@gmail.com>
2024-02-27 21:11:58 +0600

    update for new app

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,22 +2,29 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import drivers.BrowserstackDriver;
+import drivers.BrowserstackMobileDriver;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-
+import drivers.BrowserstackMobileDriver;
+import drivers.LocalMobileDriver;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestBase {
-
+    public static String deviceHost = System.getProperty("deviceHost");
     @BeforeAll
     static void beforeAll() {
-        Configuration.browser = BrowserstackDriver.class.getName();
+        switch (deviceHost) {
+            case "browserstack":
+                Configuration.browser = BrowserstackMobileDriver.class.getName();
+                break;
+            case "local":
+                Configuration.browser = LocalMobileDriver.class.getName();
+                break;
+        }
         Configuration.browserSize = null;
-        Configuration.timeout = 30000;
     }
 
     @BeforeEach
@@ -30,8 +37,13 @@ public class TestBase {
     void addAttachments() {
         String sessionId = sessionId().toString();
         Attach.pageSource();
+        if (deviceHost.equals("local")) {
+            Attach.screenshotAs("Last screenshot");
+        }
+        if (deviceHost.equals("browserstack")) {
+            Attach.addVideo(sessionId);
+        }
         closeWebDriver();
-        Attach.addVideo(sessionId);
     }
 
 }
